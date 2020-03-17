911570fa834b08f5da9057d9d76b94297e4dd2f3
phozzy <arthur.fayzullin@gmail.com>
2020-03-17 12:58:52 +0100

    Create merge function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+fn merge(first_sorted: Vec<i32>, second_sorted: Vec<i32>) -> Vec<i32> {
+    let mut output: Vec<i32> = Vec::new();
+    let mut first = first_sorted.iter();
+    let mut second = second_sorted.iter();
+    let mut f;
+    let mut s;
+    let mut fi = true;
+    let mut si = true;
+    match first.next() {
+        Some(val) => f = val,
+        None => panic!("Empy array!"),
+    };
+    match second.next() {
+        Some(val) => s = val,
+        None => panic!("Empy array!"),
+    };
+    loop {
+        if f < s && fi || !si {
+            output.push(*f);
+            match first.next() {
+                Some(val) => {
+                    f = val;
+                },
+                None => {
+                    fi = false;
+                },
+            };
+        } else {
+            output.push(*s);
+            match second.next() {
+                Some(val) => {
+                    s = val;
+                },
+                None => {
+                    si = false;
+                },
+            };
+        };
+        if !(fi || si) {
+            break output
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let second_sorted = vec![3, 5, 9];
+    let first_sorted = vec![2, 7];
+    let output = merge(first_sorted, second_sorted);
+    for it in output {
+        println!("{}", it);
+    };
 }
